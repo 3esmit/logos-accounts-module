4fd4cb0be3d281b4a9b3303f892b2b265edb2a62
Dario Gabriel Lipicar <dario@status.im>
2026-07-22 15:59:01 -0300

    bump module builder

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
     go-wallet-sdk = {
       url = "github:status-im/go-wallet-sdk/0938a704506b0ff444378045d17be9e19e699d80";
       flake = false;
