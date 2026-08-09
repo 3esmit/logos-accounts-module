884f9af1bd72374ad6a615413a5c1e10f600fae6
3esmit <3esmit@users.noreply.github.com>
2026-08-08 22:19:22 -0300

    fix(deps): refresh maintained module-builder pin

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Logos Accounts Module - An accounts plugin for Logos";
 
   inputs = {
-    logos-module-builder.url = "github:3esmit/logos-module-builder?rev=d48c2f14f9ad24b1a8b0c8fbf17620fef3ddb4e1";
+    logos-module-builder.url = "github:3esmit/logos-module-builder?rev=324b459c3f7b59171d249f3ccbcc362403b3fcaf";
     go-wallet-sdk = {
       url = "github:status-im/go-wallet-sdk/0938a704506b0ff444378045d17be9e19e699d80";
       flake = false;
